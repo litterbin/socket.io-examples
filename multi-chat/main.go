89e019203f488d1823ae0cdf42ad6cf85c6f8e50
anarcher <anarcher@gmail.com>
2015-04-14 17:39:50 +0900

    Using socket.io's room

--- multi-chat/main.go
+++ multi-chat/main.go
@@ -7,6 +7,14 @@ import (
 	"github.com/googollee/go-socket.io"
 )
 
+var rooms = []string{"channel1", "channel2", "channel3 "}
+
+type Message struct {
+	Channel string `json:"channel"`
+	Msg     string `json:"msg"`
+	Userid  int    `json:"userId"`
+}
+
 func main() {
 
 	server, err := socketio.NewServer(nil)
@@ -17,9 +25,15 @@ func main() {
 	server.On("connection", func(so socketio.Socket) {
 		log.Println("on connection")
 		so.Join("chat")
-		so.On("chat message", func(msg string) {
-			log.Println("emit:", so.Emit("chat message", msg))
-			so.BroadcastTo("chat", "chat message", msg)
+		so.On("chat message", func(msg Message) {
+			log.Println("channel:", msg.Channel, "msg:", msg.Msg)
+			so.Join(msg.Channel)
+			so.Emit("chat message", msg)
+			so.BroadcastTo(msg.Channel, "chat message", msg)
+		})
+
+		so.On("error", func() {
+			log.Println("so error")
 		})
 
 		so.On("disconnection", func() {
